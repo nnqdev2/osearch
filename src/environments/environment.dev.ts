d8c7c7e96b6555fb57b41f435fefced6c72d6177
Nga Quan <quan.nga@deq.state.or.us>
2018-09-24 16:39:55 -0700

    contacts ...

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,62 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    olprrapi: 'http://deqwebdev/olprrapi/olprr/',
+    olprrapi_state: 'http://deqwebdev/olprrapi/olprr/state',
+    olprrapi_sitetype: 'http://deqwebdev/olprrapi/olprr/sitetype',
+    olprrapi_confirmationtype: 'http://deqwebdev/olprrapi/olprr/confirmationtype',
+    olprrapi_county: 'http://deqwebdev/olprrapi/olprr/county',
+    olprrapi_discoverytype: 'http://deqwebdev/olprrapi/olprr/discoverytype',
+    olprrapi_quadrant: 'http://deqwebdev/olprrapi/olprr/quadrant',
+    olprrapi_releasecausetype: 'http://deqwebdev/olprrapi/olprr/releasecausetype',
+    olprrapi_sourcetype: 'http://deqwebdev/olprrapi/olprr/sourcetype',
+    olprrapi_streettype: 'http://deqwebdev/olprrapi/olprr/streettype',
+    olprrapi_deqoffice: 'http://deqwebdev/olprrapi/olprr/deqoffice',
+    olprrapi_incidentstatus: 'http://deqwebdev/olprrapi/olprr/incidentstatus',
+    olprrapi_tankstatus: 'http://deqwebdev/olprrapi/olprr/tankstatus',
+    olprrapi_filestatus: 'http://deqwebdev/olprrapi/olprr/filestatus',
+    olprrapi_region: 'http://deqwebdev/olprrapi/olprr/region',
+    olprrapi_city: 'http://deqwebdev/olprrapi/olprr/city',
+    olprrapi_zipcode: 'http://deqwebdev/olprrapi/olprr/zipcode',
+    olprrapi_datecompare: 'http://deqwebdev/olprrapi/olprr/datecompare',
+    olprrapi_contacttype: 'http://deqwebdev/olprrapi/olprr/contacttype',
+    olprrapi_contacttype2: 'http://deqwebdev/olprrapi/olprr/contacttype2',
+    olprrapi_cleanupsitetype: 'http://deqwebdev/olprrapi/olprr/cleanupsitetype',
+    olprrapi_projectmanager: 'http://deqwebdev/olprrapi/olprr/projectmanager',
+    olprrapi_incident: 'http://deqwebdev/olprrapi/olprr/incident',
+    olprrapi_lust_search: 'http://deqwebdev/olprrapi/review/lustsite',
+    olprrapi_ust_search: 'http://deqwebdev/olprrapi/review/ustsite',
+    olprrapi_review_search: 'http://deqwebdev/olprrapi/review',
+    olprrapi_review_incidentdatabyid: 'http://deqwebdev/olprrapi/review/incidentdatabyid/',
+    address_correction: 'https://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify',
+    olprrapi_review_postalcounty: 'http://deqwebdev/olprrapi/review/postalcounty',
+    lit_site_setup: 'http://deqwebdev/lit_maint/?appid=',
+    olprrapi_insert_lust: 'http://deqwebdev/olprrapi/review/lustincident',
+    olprrapi_sitealias: 'http://deqwebdev/olprrapi/lust/sitealias',
+    olprrapi_contact: 'http://deqwebdev/olprrapi/lust/contact',
+    // olprrapi_contact2: 'http://deqwebdev/olprrapi/lust/contact2',
+    olprrapi_postalcounty_lookup: 'http://deqwebdev/olprrapi/lust/postalcounty',
+    olprrapi_brownfield: 'http://deqwebdev/olprrapi/olprr/brownfield',
+    olprrapi_sitetype2: 'http://deqwebdev/olprrapi/olprr/sitetype2',
+    olprrapi_lust_incident_get: 'http://deqwebdev/olprrapi/lust/incident',
+    olprrapi_lust_pm: 'http://deqwebdev/olprrapi/lust/pm',
+    olprrapi_lust_incident_update: 'http://deqwebdev/olprrapi/lust/incident',
+    olprrapi_lust_detail: 'http://deqwebdev/olprrapi/lust/lustid',
+    olprrapi_lust_contact: 'http://deqwebdev/olprrapi/lust/contact',
+    olprrapi_lust_incident_delete: 'http://deqwebdev/olprrapi/lust/lustid/',
+    contractor_uid: 'INGRID',
+    contractor_pwd: 'GAFFNEY',
+    cache_expiry_in_ms: '43200000',
+    olprr_PDF_output_location: '\\DEQGRS1\LUSTDocs\\'
+  };
+
+  /*
+   * In development mode, to ignore zone related error stack frames such as
+   * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+   * import the following file, but please comment it out in production mode
+   * because it will have performance impact when throw error
+   */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
