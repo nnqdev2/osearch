9e10cce466aee773f688069be9179c469600da46
Nga Quan <quan.nga@deq.state.or.us>
2018-10-05 10:33:51 -0700

    contact component...

--- src/app/lust-search/contact/contacts/contacts-result-data-source.service.ts
+++ src/app/lust-search/contact/contacts/contacts-result-data-source.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { DataSource } from '@angular/cdk/table';
+import { ContactAffilGet } from '../../../models/contact-affil-get';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { LustDataService } from '../../../services/lust-data.service';
+import { CollectionViewer } from '@angular/cdk/collections';
+import { finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ContactsResultDataSourceService {
+export class ContactsResultDataSourceService implements DataSource<ContactAffilGet> {
 
-  constructor() { }
+  private resultsSubject = new BehaviorSubject<ContactAffilGet[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  private contacts: ContactAffilGet[];
+
+  public contactResultReturned$ = this.resultsSubject.asObservable();
+  public contactLoading$ = this.loadingSubject.asObservable();
+
+  constructor(private lustDataService: LustDataService) {}
+
+  connect(collectionViewer: CollectionViewer): Observable<ContactAffilGet[]> {
+      return this.contactResultReturned$;
+      // return this.resultsSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+      this.resultsSubject.complete();
+      this.loadingSubject.complete();
+  }
+
+  loadResults(lustId: number) {
+      console.log( JSON.stringify(lustId));
+      this.loadingSubject.next(true);
+      this.lustDataService.getLustContacts(lustId)
+          .pipe(
+              finalize(() => this.loadingSubject.next(false)),
+          )
+          .subscribe(
+              data => {
+                  this.contacts = data;
+                  this.resultsSubject.next(this.contacts);
+                  this.loadingSubject.next(false);
+              }
+          );
+  }
 }
--- src/app/lust-search/contact/contacts/contacts.component.ts
+++ src/app/lust-search/contact/contacts/contacts.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { ContactsResultDataSourceService } from '../../contact/contacts/contacts-result-data-source.service';
+import { Subscription } from 'rxjs';
+
+import { MatDialogRef, MatDialog, MatDialogConfig } from '@angular/material';
+import { ConfirmDeleteDialogComponent } from '../../confirm-delete-dialog/confirm-delete-dialog.component';
+import { LustDataService } from '../../../services/lust-data.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApGetLogNumber } from '../../../models/apGetLogNumber';
+import { ContactAffilGet } from '../../../models/contact-affil-get';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
+  private lustIdSub: any;
+  private lustId: number;
+  logNumber: string;
+  contactsDataSource: ContactsResultDataSourceService;
+  displayedColumns = ['siteNameAliasId', 'siteAliasName', 'lastChangeBy', 'lastChangeDate'];
+  private subscription: Subscription;
+  private contacts: ContactAffilGet[];
+  private totalRows = 0;
+  private confirmDeleteDialogRef: MatDialogRef<ConfirmDeleteDialogComponent, any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private lustDataService: LustDataService, private route: ActivatedRoute, private router: Router
+              , private confirmDeleteDialog: MatDialog) {
+    this.contactsDataSource = new ContactsResultDataSourceService(this.lustDataService);
   }
 
+  ngOnInit() {
+    this.lustIdSub = this.route.parent.params.subscribe(params => {
+      this.lustId = +params['lustid'];
+    });
+    this.route.data.subscribe((data: {apGetLogNumber: ApGetLogNumber}) => {
+      this.logNumber = data.apGetLogNumber.logNumber; });
+    this.loadResultPage();
+    this.getSearchResults();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.loadResultPage();
+    this.getSearchResults();
+  }
+
+  ngAfterViewInit() {
+  }
+
+  loadResultPage() {
+    this.contactsDataSource.loadResults(this.lustId);
+  }
+
+  getSearchResults() {
+    this.subscription = this.contactsDataSource.contactResultReturned$.subscribe(
+      siteAliases => {
+        this.contacts = siteAliases;
+        if (this.contacts !== undefined &&
+          this.contacts.length > 0) {
+            this.totalRows = this.contacts.length;
+        } else {
+          this.totalRows = 0;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.lustIdSub.unsubscribe();
+  }
+
+  onEdit(contact: ContactAffilGet) {
+    this.router.navigate(['../contact' , contact.affilId], {relativeTo: this.route});
+  }
+
+  // onDelete(contact: ContactAffilGet) {
+  //   const dialogConfig = new MatDialogConfig();
+  //   dialogConfig.autoFocus = true;
+  //   dialogConfig.data = {
+  //     title: 'Confirm Delete',
+  //     message1: 'Are you sure you want to delete contact ' + contact.firstName + ' ' + contact.lastName + ' ?' ,
+  //   };
+  //   dialogConfig.disableClose =  true;
+  //   this.confirmDeleteDialogRef = this.confirmDeleteDialog.open(ConfirmDeleteDialogComponent, dialogConfig);
+  //   this.confirmDeleteDialogRef.afterClosed().subscribe(result => {
+  //     if (result === 'confirm') {
+  //       this.lustDataService.delSiteAlias(contact.siteNameAliasId).subscribe();
+  //       this.loadResultPage();
+  //       this.getSearchResults();
+  //     }
+  //   });
+  // }
+
+  add() {
+    this.router.navigate(['../contact'], {relativeTo: this.route});
+  }
+
+  refresh() {
+    this.loadResultPage();
+    this.getSearchResults();
+  }
 }
